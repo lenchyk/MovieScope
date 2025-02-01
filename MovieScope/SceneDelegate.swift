51399f1a9d57ba5bc597a1f4255359d40002ae16
Lena Soroka <4ta.lenasoroka.1989@gmail.com>
2025-02-01 20:32:47 +0200

    Initial Commit

---
+++ MovieScope/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  MovieScope
+//
+//  Created by Lena Soroka on 01.02.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ MovieScope/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MovieScope
+//
+//  Created by Lena Soroka on 01.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
+
